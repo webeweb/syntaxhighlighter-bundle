3323a3367f8a589f63b7baece72b650796e8cdfe
webeweb <support@webeweb.fr>
2017-11-04 15:25:57 +0100

    Add main bundle class

---
+++ WBWSyntaxHighlighterBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the WBWSyntaxHighlighterBundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SyntaxHighlighterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * SyntaxHighlighter bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\SyntaxHighlighterBundle
+ * @final
+ */
+final class WBWSyntaxHighlighterBundle extends Bundle {
+
+}
