4ad564659bd9dea304b9f53fa84716cf59954920
webeweb <support@webeweb.fr>
2018-04-24 10:30:31 +0200

    Remove final keyword

--- API/SyntaxHighlighterStrings.php
+++ API/SyntaxHighlighterStrings.php
@@ -18,9 +18,8 @@ use WBW\Bundle\SyntaxHighlighterBundle\Encoder\SyntaxHighlighterEncoder;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\SyntaxHighlighterBundle\API
- * @final
  */
-final class SyntaxHighlighterStrings {
+class SyntaxHighlighterStrings {
 
     /**
      * Alert.
