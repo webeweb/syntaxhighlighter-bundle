79819c29f87847e28e579fff88e2d7df9b7e0573
webeweb <support@webeweb.fr>
2018-06-19 22:19:39 +0200

    Support API classes into Twig extension

--- Twig/Extension/AbstractSyntaxHighlighterTwigExtension.php
+++ Twig/Extension/AbstractSyntaxHighlighterTwigExtension.php
@@ -12,6 +12,9 @@
 namespace WBW\Bundle\SyntaxHighlighterBundle\Twig\Extension;
 
 use Twig_Extension;
+use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterConfig;
+use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterDefaults;
+use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterStrings;
 use WBW\Library\Core\Utility\Argument\StringUtility;
 
 /**
@@ -31,16 +34,38 @@ abstract class AbstractSyntaxHighlighterTwigExtension extends Twig_Extension {
     }
 
     /**
-     * Displays a SyntaxHighlighter.
+     * Displays a SyntaxHighlighter config.
+     *
+     * @param SyntaxHighlighterConfig $config The SyntaxHighlighter configuration.
+     * @return string Returns the SyntaxHighlighter config.
+     */
+    protected function syntaxHighlighterConfig(SyntaxHighlighterConfig $config) {
+
+        // Initialize the template.
+        $template = [];
+
+        $template[] = "SyntaxHighlighter.config.bloggerMode = " . StringUtility::parseBoolean($config->getBloggerMode()) . ";";
+        $template[] = "SyntaxHighlighter.config.stripBrs = " . StringUtility::parseBoolean($config->getStripBrs()) . ";";
+        $template[] = "SyntaxHighlighter.config.tagName = \"" . $config->getTagName() . "\";";
+        if (null !== $config->getStrings()) {
+            $template[] = $this->syntaxHighlighterStrings($config->getStrings());
+        }
+
+        // Return the HTML.
+        return implode("\n", $template);
+    }
+
+    /**
+     * Displays a SyntaxHighlighter content.
      *
      * @param string $language The language.
      * @param string $content The content.
-     * @return string Returns the SyntaxHightlighter.
+     * @return string Returns the SyntaxHightlighter content.
      */
-    protected function syntaxHighlighter($language, $content) {
+    protected function syntaxHighlighterContent($tag, $language, $content) {
 
         // Initialize the template.
-        $template = "<pre %attributes%>\n%innerHTML%\n</pre>";
+        $template = "<%tag% %attributes%>\n%innerHTML%\n</%tag%>";
 
         // Initialize the attributes.
         $attributes = [];
@@ -49,7 +74,58 @@ abstract class AbstractSyntaxHighlighterTwigExtension extends Twig_Extension {
         $attributes["class"][] = null !== $language ? $language : "php";
 
         // Return the HTML.
-        return StringUtility::replace($template, ["%attributes%", "%innerHTML%"], [StringUtility::parseArray($attributes), htmlentities($content)]);
+        return StringUtility::replace($template, ["%tag%", "%attributes%", "%innerHTML%"], [$tag, StringUtility::parseArray($attributes), htmlentities($content)]);
+    }
+
+    /**
+     * Displays a SyntaxHighlighter defaults.
+     *
+     * @param SyntaxHighlighterDefaults $defaults The SyntaxHighlighter defaults.
+     * @return string Returns the SyntaxHighlighter defaults.
+     */
+    protected function syntaxHighlighterDefaults(SyntaxHighlighterDefaults $defaults) {
+
+        // Initialize the template.
+        $template = [];
+
+        $template[] = "SyntaxHighlighter.defaults['auto-links'] = " . StringUtility::parseBoolean($defaults->getAutoLinks()) . ";";
+        $template[] = "SyntaxHighlighter.defaults['class-name'] = \"" . $defaults->getClassName() . "\";";
+        $template[] = "SyntaxHighlighter.defaults['collapse'] = " . StringUtility::parseBoolean($defaults->getCollapse()) . ";";
+        $template[] = "SyntaxHighlighter.defaults['first-line'] = " . $defaults->getFirstLine() . ";";
+        $template[] = "SyntaxHighlighter.defaults['gutter'] = " . StringUtility::parseBoolean($defaults->getGutter()) . ";";
+        $template[] = "SyntaxHighlighter.defaults['highlight'] = [" . implode(",", $defaults->getHighlight()) . "];";
+        $template[] = "SyntaxHighlighter.defaults['html-script'] = " . StringUtility::parseBoolean($defaults->getHtmlScript()) . ";";
+        $template[] = "SyntaxHighlighter.defaults['smart-tabs'] = " . StringUtility::parseBoolean($defaults->getSmartTabs()) . ";";
+        $template[] = "SyntaxHighlighter.defaults['tab-size'] = " . $defaults->getTabSize() . ";";
+        $template[] = "SyntaxHighlighter.defaults['toolbar'] = " . StringUtility::parseBoolean($defaults->getToolbar()) . ";";
+
+        // Return the HTML.
+        return implode("\n", $template);
+    }
+
+    /**
+     * Displays a SyntaxHighlighter strings.
+     *
+     * @param SyntaxHighlighterStrings $strings The SyntaxHighlighter strings.
+     * @return string Returns the SyntaxHighlighter strings.
+     */
+    protected function syntaxHighlighterStrings(SyntaxHighlighterStrings $strings) {
+
+        // Initialize the template.
+        $template = [];
+
+        $template[] = "SyntaxHighlighter.config.strings.alert = \"" . $strings->getAlert() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.brushNotHtmlScript = \"" . $strings->getBrushNotHtmlScript() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.copyToClipboard = \"" . $strings->getCopyToClipboard() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.copyToClipboardConfirmation = \"" . $strings->getCopyToClipboardConfirmation() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.expandSource = \"" . $strings->getExpandSource() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.help = \"" . $strings->getHelp() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.noBrush = \"" . $strings->getNoBrush() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.print = \"" . $strings->getPrint() . "\";";
+        $template[] = "SyntaxHighlighter.config.strings.viewSource = \"" . $strings->getViewSource() . "\";";
+
+        // Return the HTML.
+        return implode("\n", $template);
     }
 
 }
