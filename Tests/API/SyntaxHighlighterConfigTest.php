00f6e8c6acbbaa2af71aab96e5dc5dfbe9a2e34f
webeweb <support@webeweb.fr>
2018-06-19 22:17:11 +0200

    Remove toString() unti test

--- Tests/API/SyntaxHighlighterConfigTest.php
+++ Tests/API/SyntaxHighlighterConfigTest.php
@@ -13,7 +13,6 @@ namespace WBW\Bundle\SyntaxHighlighterBundle\Tests\API;
 
 use PHPUnit_Framework_TestCase;
 use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterConfig;
-use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterStrings;
 
 /**
  * SyntaxHighlighter config test.
@@ -37,47 +36,6 @@ final class SyntaxHighlighterConfigTest extends PHPUnit_Framework_TestCase {
         $this->assertNull($obj->getStrings());
         $this->assertFalse($obj->getStripBrs());
         $this->assertEquals("pre", $obj->getTagName());
-    }
-
-    /**
-     * Tests the __toString() method.
-     *
-     * @return void
-     */
-    public function testToString() {
-
-        $obj = new SyntaxHighlighterConfig();
-
-        $obj->setBloggerMode(true);
-        $obj->setStripBrs(true);
-        $obj->setTagName("blocquote");
-
-        $res0 = <<<'EOTXT'
-SyntaxHighlighter.config.bloggerMode = true;
-SyntaxHighlighter.config.stripBrs = true;
-SyntaxHighlighter.config.tagName = "blocquote";
-
-EOTXT;
-
-        $this->assertEquals($res0, (string) $obj);
-
-        $obj->setStrings(new SyntaxHighlighterStrings());
-
-        $res9 = $res0 . <<<'EOTXT'
-SyntaxHighlighter.config.strings.alert = "SyntaxHighlighter
-
-";
-SyntaxHighlighter.config.strings.brushNotHtmlScript = "Brush wasn't made for html-script option:";
-SyntaxHighlighter.config.strings.copyToClipboard = "copy to clipboard";
-SyntaxHighlighter.config.strings.copyToClipboardConfirmation = "The code is in your clipboard now";
-SyntaxHighlighter.config.strings.expandSource = "+ expand source";
-SyntaxHighlighter.config.strings.help = "?";
-SyntaxHighlighter.config.strings.noBrush = "Can't find brush for:";
-SyntaxHighlighter.config.strings.print = "print";
-SyntaxHighlighter.config.strings.viewSource = "view source";
-EOTXT;
-
-        $this->assertEquals($res9, (string) $obj);
     }
 
 }
