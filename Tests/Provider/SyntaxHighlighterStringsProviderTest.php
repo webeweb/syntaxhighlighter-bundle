b7669fa2e2e4ebb46d0739a438977a6135bb1306
webeweb <support@webeweb.fr>
2018-06-23 11:10:58 +0200

    Add SyntaxHighlighter strings provider unit test

---
+++ Tests/Provider/SyntaxHighlighterStringsProviderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the syntaxhighligter-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SyntaxHighlighterBundle\Tests\Provider;
+
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\Translation\TranslatorInterface;
+use WBW\Bundle\SyntaxHighlighterBundle\API\SyntaxHighlighterStrings;
+use WBW\Bundle\SyntaxHighlighterBundle\Provider\SyntaxHighlighterStringsProvider;
+
+/**
+ * SyntaxHighlighter strings provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SyntaxHighlighterBundle\Tests\Provider
+ * @final
+ */
+final class SyntaxHighlighterStringsProviderTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Translator.
+     *
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @{inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Translator mock.
+        $this->translator = $this->getMockBuilder(TranslatorInterface::class)->getMock();
+        $this->translator->expects($this->any())->method("getLocale")->willReturn("en");
+        $this->translator->expects($this->any())->method("trans")->willReturn("");
+    }
+
+    /**
+     * Tests the getSyntaxHighlighterStrings() method.
+     *
+     * @return void
+     */
+    public function testGetSyntaxHighlighterStrings() {
+
+        $obj = new SyntaxHighlighterStringsProvider($this->translator);
+
+        $this->assertInstanceOf(SyntaxHighlighterStrings::class, $obj->getSyntaxHighlighterStrings());
+    }
+
+}
