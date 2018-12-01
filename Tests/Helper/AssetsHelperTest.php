34c009dbe85d310f4dba4590bc75c3d4ed88287f
webeweb <support@webeweb.fr>
2018-12-01 14:17:30 +0100

    Move SyntaxHighlighter 3.83 into assets directory

---
+++ Tests/Helper/AssetsHelperTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the syntaxhighlighter-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SyntaxHighlighterBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Helper\TestAssetsHelper;
+use WBW\Bundle\SyntaxHighlighterBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Assets helper test
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SyntaxHighlighterBundle\Tests\Helper
+ */
+class AssetsHelperTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Directory "assets".
+     *
+     * @var string
+     */
+    private $directoryAssets;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set the directories.
+        $this->directoryAssets = getcwd() . "/Resources/assets";
+    }
+
+    /**
+     * Tests the listAssets() method.
+     *
+     * @return void
+     */
+    public function testListAssets() {
+
+        $res = TestAssetsHelper::listAssets($this->directoryAssets);
+        $this->assertCount(1, $res);
+
+        $this->assertRegexp("/syntaxhighlighter\-.*\.zip$/", $res[0]);
+    }
+
+}
