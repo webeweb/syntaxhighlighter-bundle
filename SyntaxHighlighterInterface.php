8adc2037e610cac3244fa50561b5008d5ae38d99
webeweb <support@webeweb.fr>
2019-04-15 19:29:09 +0200

    Add SyntaxHighlighter interface

---
+++ SyntaxHighlighterInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the syntaxhighligter-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SyntaxHighlighterBundle;
+
+/**
+ * SyntaxHighlighter interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SyntaxHighlighterBundle
+ */
+interface SyntaxHighlighterInterface {
+
+    /**
+     * SyntaxHighlighter version.
+     *
+     * @var string
+     */
+    const SYNTAXHIGHLIGHTER_VERSION = "3.0.83";
+}
---
+++ Tests/SyntaxHighlighterInterfaceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the syntaxhighligter-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\SyntaxHighlighterBundle\Tests;
+
+use WBW\Bundle\SyntaxHighlighterBundle\SyntaxHighlighterInterface;
+
+/**
+ * SyntaxHighlighter interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\SyntaxHighlighterBundle\Tests
+ */
+class SyntaxHighlighterInterfaceTest {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("3.0.83", SyntaxHighlighterInterface::SYNTAXHIGHLIGHTER_VERSION);
+    }
+}
